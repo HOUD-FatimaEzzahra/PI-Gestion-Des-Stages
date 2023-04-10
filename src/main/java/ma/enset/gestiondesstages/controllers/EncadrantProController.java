8a6cd82cecbbed6f80b4462bbabd9f17f49aeb66
ManarElhouda <manarbadre13@gmail.com>
2023-04-10 03:52:48 +0000

    Services Repository done

---
+++ src/main/java/ma/enset/gestiondesstages/controllers/EncadrantProController.java
@@ -0,0 +1,16 @@
+package ma.enset.gestiondesstages.controllers;
+
+import ma.enset.gestiondesstages.models.EncadrantProfessionnel;
+import ma.enset.gestiondesstages.repositories.EncadrantProRepository;
+import ma.enset.gestiondesstages.services.EncadrantProService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class EncadrantProController{
+
+    
+}
--- src/main/java/ma/enset/gestiondesstages/models/EncadrantProfessionnel.java
+++ src/main/java/ma/enset/gestiondesstages/models/EncadrantProfessionnel.java
@@ -12,4 +12,28 @@ public class EncadrantProfessionnel extends Encadrant{
     private String poste;
     @OneToMany(mappedBy = "encadrantProfessionnel", fetch = FetchType.LAZY)
     private List<Stage> stages;
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getPoste() {
+        return poste;
+    }
+
+    public void setPoste(String poste) {
+        this.poste = poste;
+    }
+
+    public List<Stage> getStages() {
+        return stages;
+    }
+
+    public void setStages(List<Stage> stages) {
+        this.stages = stages;
+    }
 }
