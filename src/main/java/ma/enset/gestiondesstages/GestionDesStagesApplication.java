144c37ce939dbd169602a2f8988bbd36a70c8ef4
NAJAH <mohamednajah2424@gmail.com>
2023-04-14 18:39:43 +0000

    Create EncadrantAcademique Backend

--- src/main/java/ma/enset/gestiondesstages/GestionDesStagesApplication.java
+++ src/main/java/ma/enset/gestiondesstages/GestionDesStagesApplication.java
@@ -1,7 +1,12 @@
 package ma.enset.gestiondesstages;
 
+import ma.enset.gestiondesstages.models.Encadrant;
+import ma.enset.gestiondesstages.models.EncadrantAcademique;
+import ma.enset.gestiondesstages.repositories.E_AcademiqueRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GestionDesStagesApplication {
@@ -9,5 +14,12 @@ public class GestionDesStagesApplication {
     public static void main(String[] args) {
         SpringApplication.run(GestionDesStagesApplication.class, args);
     }
+    @Bean
+    CommandLineRunner commandLineRunner(E_AcademiqueRepository eAcademiqueRepository){
+        return args -> {
+           eAcademiqueRepository.save(new EncadrantAcademique("test","najah","najah","test",null,null));
+        };
+    }
+
 
 }
